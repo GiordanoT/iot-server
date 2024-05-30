7cbe21a0e7a8f87896bc826947805128822eac29
GiordanoT <giordanotinella@gmail.com>
2024-05-30 10:31:51 +0200

    added socket.io

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,55 @@
+import http from 'http';
 import mqtt from 'mqtt';
 import Action from './data/Action';
+import {Server, Socket} from 'socket.io';
 
+/* MQTT Broker */
 const brokerUrl = 'mqtt://localhost:1883';
-const client = mqtt.connect(brokerUrl);
+const broker = mqtt.connect(brokerUrl);
 
-
-client.on('connect', () => {
-    console.log('Connection Done');
-    client.subscribe('sensors/#');
-    // client.publish('sensors/1', 'Hello I am 1');
-    // client.publish('sensors/2', 'Hello I am 2');
-    // client.end();
+broker.on('connect', () => {
+    console.log('Connection to Broker Done');
+    broker.subscribe('#');
+});
+broker.on('end', () => {
+    console.log('Disconnection to Broker Done');
+});
+broker.on('error', (error) => {
+    console.error('Broker error:', error);
 });
 
-client.on('end', () => {
-    console.log('Disconnection Done');
-});
+/* Web Socket */
+const PORT = 5003;
+const server = http.createServer();
+const io = new Server(server, {path: '/iot'});
+server.listen(PORT);
+console.log(`Server Listening on port ${PORT}.`);
 
-client.on('error', (error) => {
-    console.error('MQTT client error:', error);
-});
 
-client.on('message', (topic, message) => {
-    // console.log(`Received message on topic "${topic}": ${message.toString()}`);
-    try {
-        const value = JSON.parse(message.toString());
-        const action = Action.SET_ROOT_FIELD(`topics.${topic}`, '+=', value, false);
-        console.log(action);
-    } catch (e) {
-        console.log(topic, e)
-    }
+io.on('connection', async(socket: Socket) => {
+    const projectID = socket.handshake.query.project as string;
+    socket.join(projectID);
+    console.log(`New User Connected to Project: ${projectID}`);
+    const topics: string[] = [];
+    broker.on('message', (topic, message) => {
+        try {
+            if(!topics.includes(topic)) {
+                topics.push(topic);
+                const initAction = Action.SET_ROOT_FIELD(`topics.${topic}`, '=', [], false);
+                socket.broadcast.to(projectID).emit('pullAction', initAction);
+            }
+            const value = JSON.parse(message.toString());
+            const action = Action.SET_ROOT_FIELD(`topics.${topic}`, '+=', value, false);
+            socket.broadcast.to(projectID).emit('pullAction', action);
+            console.log(topics);
+        } catch (e) {
+            console.log(topic, e)
+        }
+    });
+
+    socket.on('disconnect', () => {
+        socket.leave(projectID);
+        console.log(`${socket.id} Disconnection`);
+    });
+
 });
--- src/common/types.ts
+++ src/common/types.ts
@@ -1,4 +1,4 @@
 export type Dictionary = {[key: string] : unknown};
-export type Primitive = number|boolean|string|Dictionary;
+export type Primitive = number|boolean|string|Dictionary|unknown;
 export type ActionType = 'SET_ROOT_FIELD'|'SET_ME_FIELD'|'CREATE_ELEMENT'|'DELETE_ELEMENT';
 export type ActionOperator = '='|'+='|'-=';
