11660153f8e8a881560bd7497ee4834a92813479
GiordanoT <giordanotinella@gmail.com>
2024-07-30 11:42:59 +0200

    Bug fixing: multiple connection instances on same room

--- src/app.ts
+++ src/app.ts
@@ -3,21 +3,6 @@ import mqtt from 'mqtt';
 import Action from './data/Action';
 import {Server, Socket} from 'socket.io';
 
-/* MQTT Broker */
-const brokerUrl = 'mqtt://localhost:1883';
-const broker = mqtt.connect(brokerUrl);
-
-broker.on('connect', () => {
-    console.log('Connection to Broker Done');
-    broker.subscribe('#');
-});
-broker.on('end', () => {
-    console.log('Disconnection to Broker Done');
-});
-broker.on('error', (error) => {
-    console.error('Broker error:', error);
-});
-
 /* Web Socket */
 const PORT = 5003;
 const server = http.createServer();
@@ -27,25 +12,52 @@ const io = new Server(server, {
 });
 server.listen(PORT);
 console.log(`Server Listening on port ${PORT}.`);
+const IDs: string[] = [];
 
 
 io.on('connection', async(socket: Socket) => {
     const projectID = socket.handshake.query.project as string;
-    socket.join(projectID);
+    const brokerUrl = socket.handshake.query.brokerUrl as string;
+
+    await socket.join(projectID);
+    // Send connection to middleware OK
     console.log(`New User Connected to Project: ${projectID}`);
+
+    const ID = `${projectID}-${brokerUrl}`;
+    if(IDs.includes(ID)) return;
+    IDs.push(ID);
+
+    /* MQTT Broker */
+    const broker = mqtt.connect(brokerUrl);
+
+    broker.on('connect', () => {
+        console.log('Connection to Broker Done');
+        // send connection to broker OK
+        broker.subscribe('#');
+    });
+    broker.on('end', () => {
+        // send disconnection to Broker OK
+        console.log('Disconnection to Broker Done');
+    });
+    broker.on('error', (error) => {
+        // Send error with Broker
+        console.error('Broker error:', error);
+    });
+
     broker.on('message', (topic, message) => {
         try {
             const value = JSON.parse(message.toString());
             const action = Action.SET_ROOT_FIELD(`topics.${topic}`, '+=', value, false);
-            socket.broadcast.to(projectID).emit('pull-action', action);
-            console.log(action);
+            socket.to(projectID).emit('pull-action', action);
+            console.log(`Sending ${action.id}`);
         } catch (e) {
-            console.log(topic, e)
+            console.log(topic, e);
         }
     });
 
-    socket.on('disconnect', () => {
-        socket.leave(projectID);
+    socket.on('disconnect', async () => {
+        await socket.leave(projectID);
+        // Send disconnection to middleware OK
         console.log(`${socket.id} Disconnection`);
     });
 
